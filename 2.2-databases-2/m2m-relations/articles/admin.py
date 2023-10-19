eee9b3c8aed7c943b2077dcae035835ccf922362
Ilya Shevelev <robotedu@yandex.ru>
2023-10-19 15:16:24 +0300

    feat: hometask completed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class RelationInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tag_qty = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            print(form.cleaned_data['is_main'])
+            if form.cleaned_data['is_main'] is True:
+                main_tag_qty += 1
+            #     # if (main_tag_qty == 0):
+            #     #     main_tag_name = form.cleaned_data['tag']
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            print(main_tag_qty)
+        if main_tag_qty != 1:
+            raise ValidationError('Ошибка формы (проверьте количество главных тегов)')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class RelationInline(admin.TabularInline):
+    model = Scope
+    formset = RelationInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    # pass
+    list_display = ['title', 'text', 'published_at', 'image']
+    inlines = [RelationInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    # pass
+    list_display = ['name']
+    inlines = [RelationInline]
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,45 @@
 from django.db import models
 
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=256, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['-name']
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    # tag = models.ManyToManyField(Tag, related_name='scopes', through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(verbose_name='Главный тег')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['-is_main', '-tag']
+
+    def __str__(self):
+        return '{0}_{1}_{2}'.format(self.article, self.tag, self.is_main)
